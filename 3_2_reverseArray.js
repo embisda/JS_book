7053ec24fea7b0fa6a2aac7c67d94471468c2298
Igor Vakhromeev <usfame@gmail.com>
2017-04-08 16:02:50 +0300

    Update 3_2_reverseArray.js

--- 3_2_reverseArray.js
+++ 3_2_reverseArray.js
@@ -1,17 +1,19 @@
 function reverseArray(array) {
-	array.reverse();
-  return array;
+  var newArray = [];
+  for (var i = array.length - 1; i >= 0; i--)
+    newArray.push(array[i]);
+  return newArray;
 }
-console.log(reverseArray([1,5]));
+console.log(reverseArray([1,2,3,4,5]));
 
 function reverseArrayInPlace(myArray) {
-	var newArray = [];
+  var newArray = [];
   var lastElem = '';  
   
-	while(myArray.length>0) {
-  	lastElem = myArray[myArray.length - 1]; 
+  while(myArray.length>0) {
+    lastElem = myArray[myArray.length - 1]; 
     newArray.push(lastElem);
-  	var i = myArray.length;
+    var i = myArray.length;
     myArray.splice(i-1,1); 
   }  
   return newArray;
